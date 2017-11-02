8e8fb2a660964c8f136e81df71e088ed978b205a
broshan1 <phorton@softstackfactory.org>
2017-11-02 02:19:04 +0000

    Roshan's lesson11.js changes

--- lesson10.js
+++ lesson10.js
@@ -5,16 +5,16 @@ var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
     { name: "Betty", age: 37 }
-    ]
+    ];
 
 var nameFilter = function(filteredName) {
     for (var i = 0; i < people.length; i++) {
-        let name = people[i].name
-        if (people != filteredName) {
+        let name = people[i].name;
+        if (name != filteredName) {
             console.log("My Name is " + name)
         }
 
     }
-}
+};
 
-nameFilter("Betty")
+nameFilter("Betty");
